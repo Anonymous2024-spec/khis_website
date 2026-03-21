69e9ad249189455feb1c369d32117909903364df
Naana Shifah <shifahnana2@gmail.com>
2026-03-21 15:23:05 +0300

    chore: initialize React project with Vite, Tailwind CSS, and folder structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1 className="text-3xl font-bold text-blue-600">Hello</h1>
+        
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
